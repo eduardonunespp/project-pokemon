b11cb8e793908e601439ed416444fcfb88c699ad
Carlos Eduardo <carlospn200406@gmail.com>
2022-11-07 12:12:56 -0300

    .

---
+++ js/main.js
@@ -0,0 +1,3 @@
+import openModal from "./modules/modal.js";
+  
+openModal()
---
+++ js/modules/modal.js
@@ -0,0 +1,29 @@
+export default function openModal(){
+
+}
+
+const cardPokemon = document.querySelectorAll('.js-open-detals-pokemon')
+const modal = document.querySelector('.modal')
+const close = document.querySelector('.modal .close')
+const box = document.querySelector('.modal .box')
+
+console.log(cardPokemon)
+
+    function showModal(event){
+        modal.classList.add('open-modal')
+        console.log('nsmkwsk')
+        box.classList.add('animationModal')       
+
+    }
+
+
+    function closeModal(){
+        modal.classList.remove('open-modal')
+    }
+
+
+cardPokemon.forEach(card => {
+     card.addEventListener('click', showModal)
+ })
+
+ close.addEventListener('click', closeModal)
